9bd5f9f00deddf90cdb9504579278f2c81fbb282
Elizabet Oliveira <elizabet.oliveira@gmail.com>
2017-02-11 20:49:54 +0000

    Babel preset stage 2

--- src/js/components/FloatingButton.js
+++ src/js/components/FloatingButton.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const FloatingButton = ({ onClick, type, icon }) => (
+const FloatingButton = ({ onClick, type, icon,  ...attrs }) => (
   <span
     className={`btn-floating btn-floating--${type}`}
     onClick={onClick}
+    {...attrs}
   >
     <i className={icon}></i>
   </span>
