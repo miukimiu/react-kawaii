87a0a5ed811d88ddefed7ef1de840c022719677c
Elizabet Oliveira <elizabet.oliveira@gmail.com>
2017-03-09 22:15:25 +0000

    Removed unused files

--- src/js/actions/index.js
+++
@@ -1,8 +0,0 @@
-export const MY_ACTION = 'MY_ACTION';
-
-export function myAction(payload) {
-	return {
-		type: MY_ACTION,
-		payload: payload
-	}
-}
--- src/js/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux'
-
-const rootReducer = combineReducers({
-	routing: routerReducer
-});
-
-export default rootReducer;
