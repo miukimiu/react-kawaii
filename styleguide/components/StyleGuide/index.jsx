688e636062cceed685c260eeec36f3389c9e4348
Elizabet Oliveira <elizabet.oliveira@gmail.com>
2017-04-07 01:06:28 +0100

    Github star button

--- styleguide/components/StyleGuide/index.jsx
+++ styleguide/components/StyleGuide/index.jsx
@@ -1,39 +1,42 @@
-import { PropTypes } from 'react';
-
+import {PropTypes} from 'react';
+import GitHubButton from 'react-github-button';
 import './sass/app.scss';
 
+const StyleGuideRenderer = ({title, components, toc, sidebar}) => (
 
-const StyleGuideRenderer = ({ title, components, toc, sidebar }) => (
+  <div className="kawaii-styleguide">
+    <main className="main">
 
-        <div className="kawaii-styleguide">
-            <main className="main">
+      {sidebar && (
+        <nav className="main__nav">
+          <h3 className="h3">React Kawaii</h3>
 
-                {sidebar &&
-                    <nav className="main__nav">
-                        <h3 className="h3">React Kawaii</h3>
-                        {toc}
-                    </nav>
-                }
-                <div className="main__content">
+          <div className="main__nav__buttons">
+            <GitHubButton type="stargazers" size="small" namespace="miukimiu" repo="react-kawaii"/>
+          </div>
+          {toc}
+        </nav>
+      )}
+      <div className="main__content">
+        {components}
 
-                    {components}
+      </div>
+    </main>
 
-                </div>
-            </main>
-
-            <footer className="footer">
-                <p>Generated with <a href="https://github.com/styleguidist/react-styleguidist">React Styleguidist</a></p>
-            </footer>
-        </div>
-
+    <footer className="footer">
+      <p>Generated with
+        <a href="https://github.com/styleguidist/react-styleguidist">React Styleguidist</a>
+      </p>
+    </footer>
+  </div>
 
 );
 
 StyleGuideRenderer.propTypes = {
-    title: PropTypes.string.isRequired,
-    components: PropTypes.object.isRequired,
-    toc: PropTypes.node.isRequired,
-    sidebar: PropTypes.bool,
+  title: PropTypes.string.isRequired,
+  components: PropTypes.object.isRequired,
+  toc: PropTypes.node.isRequired,
+  sidebar: PropTypes.bool
 };
 
 export default StyleGuideRenderer;
