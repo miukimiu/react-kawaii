b03f6fdb410ac6102b15130f2db66dd96ee53228
Elizabet Oliveira <elizabet.oliveira@gmail.com>
2018-01-20 02:22:04 +0000

    react-styleguidist 6.1.0

---
+++ docs/Introduction.md
@@ -0,0 +1,4 @@
+- [GitHub repository](https://github.com/miukimiu/react-kawaii/)
+- [NPM page](https://www.npmjs.com/package/react-kawaii)
+
+React Kawaii is a library of cute SVG illustrations (react components). Ideal if you want to give some cuteness and personality to your react application.
--- webpack.config.js
+++ webpack.config.js
@@ -8,39 +8,31 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'lib'),
     filename: 'index.js',
-     library: ["react-kawaii"],
-     libraryTarget: 'umd',
-     publicPath: '/lib/',
+    library: ['react-kawaii'],
+    libraryTarget: 'umd',
+    publicPath: '/lib/',
   },
   module: {
     rules: [{
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: [
-          'babel-loader',
-        ],
+        use: ['babel-loader'],
       },
       {
         test: /\.scss$/,
         exclude: /node_modules/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(ttf|eot|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-        use: [
-          'file-loader'
-        ]
+        use: ['file-loader'],
       },
       {
         test: /\.(jpg|png|gif|svg)$/i,
         use: [
           'file-loader?name=[path][name].[ext]?[hash]&outputPath=/assets/&publicPath=/assets/'
-        ]
-      }
+        ],
+      },
     ]
   },
   resolve: {
@@ -50,10 +42,8 @@ module.exports = {
     new webpack.LoaderOptionsPlugin({
       options: {
         context: __dirname,
-        postcss: [
-          autoprefixer,
-        ],
+        postcss: [autoprefixer],
       },
     }),
-  ]
+  ],
 };
