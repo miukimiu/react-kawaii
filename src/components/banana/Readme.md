666594ad608f245585bf583f6e5e4b65bcc1319b
Karin Hendrikse <k.hendrikse@youngcapital.nl>
2018-10-04 23:21:00 +0200

    Created a banana component

---
+++ src/components/banana/Readme.md
@@ -0,0 +1,5 @@
+In this example the _Banana_ has the mood <b>excited</b>. You can see the code and play with it. Try to change the mood to any other.
+
+```
+<Banana size={300} mood="excited" color="#FFD600" />
+```
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Ghost from './components/ghost/Ghost';
 import IceCream from './components/iceCream/IceCream';
 import Mug from './components/mug/Mug';
 import Planet from './components/planet/Planet';
+import Banana from './components/banana/Banana';
 import SpeechBubble from './components/speechBubble/SpeechBubble';
 
-export { Browser, CreditCard, Ghost, IceCream, Mug, Planet, SpeechBubble };
+export { Banana, Browser, CreditCard, Ghost, IceCream, Mug, Planet, SpeechBubble };
