7f01bfbcd7fffc5f430e7b62d9c669a8282723e7
colorfulfool <t0ny.hamster@gmail.com>
2019-01-22 20:42:46 +0300

    Fix import

--- src/components/alert/Alert.jsx
+++ src/components/alert/Alert.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Browser } from '../browser/Browser';
+import Browser from '../browser/Browser';
 
 const BootstrapAlert = styled.div`
   display: flex;
