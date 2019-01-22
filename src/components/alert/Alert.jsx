77295f05f2d0d1dc61e4bfdf4ef453f5ae66a265
colorfulfool <t0ny.hamster@gmail.com>
2019-01-22 19:16:02 +0300

    Move to styled-components, rename a constant

--- src/components/alert/Alert.jsx
+++ src/components/alert/Alert.jsx
@@ -1,57 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { Browser } from '../browser/Browser';
 
-const moodForStyle = {
+const BootstrapAlert = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  
+  border: 1px solid transparent;
+  border-radius: 4px;
+  margin-bottom: 20px;
+  padding: 15px;
+  
+  &.danger {
+    background-color: #f2dede;
+    border-color: #ebccd1;
+    color: #a94442;
+  }
+  
+  &.warning {
+    background-color: #fcf8e3;
+    border-color: #faebcc;
+    color: #8a6d3b;
+  }
+  
+  &.success {
+    background-color: #dff0d8;
+    border-color: #d6e9c6;
+    color: #3c763d;
+  }
+  
+  &.info {
+    background-color: #d9edf7;
+    border-color: #bce8f1;
+    color: #31708f;
+  }
+`
+
+const bsStyleToMood = {
   "danger": "shocked",
   "warning": "sad",
   "success": "blissful",
   "info": "excited"
 };
 
-const layout = {
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'center',
-  alignItems: 'center'
-};
-
-const alert = {
-  border: '1px solid transparent',
-  borderRadius: '4px',
-  marginBottom: '20px',
-  padding: '15px'
-};
-
-const alertForStyle = {
-  "danger": {
-    backgroundColor: '#f2dede',
-    borderColor: '#ebccd1',
-    color: '#a94442'
-  },
-  "warning": {
-    backgroundColor: '#fcf8e3',
-    borderColor: '#faebcc',
-    color: '#8a6d3b'
-  },
-  "success": {
-    backgroundColor: '#dff0d8',
-    borderColor: '#d6e9c6',
-    color: '#3c763d'
-  },
-  "info": {
-    backgroundColor: '#d9edf7',
-    borderColor: '#bce8f1',
-    color: '#31708f'
-  } 
-};
-
 const Alert = ({ bsStyle, children }) => (
-  <div style={{...layout, ...alert, ...alertForStyle[bsStyle]}}>
-    <Browser mood={moodForStyle[bsStyle]} size={80} />
+  <BootstrapAlert className={bsStyle}>
+    <Browser mood={bsStyleToMood[bsStyle]} size={80} />
     <div style={{width: '15px'}}></div>
     {children}
-  </div>
+  </BootstrapAlert>
 );
 
 Alert.propTypes = {
