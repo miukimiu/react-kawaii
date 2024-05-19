a16f15e483204cf85f115d3e29786502729a6684
Elizabet Oliveira <2750668+miukimiu@users.noreply.github.com>
2024-05-19 13:41:46 +0100

    toolbar button adjustemnts

--- apps/website/src/components/kawaiiList/KawaiiListToolbar.tsx
+++ apps/website/src/components/kawaiiList/KawaiiListToolbar.tsx
@@ -1,9 +1,11 @@
 'use client';
 
-import { Flex, Select, Slider } from '@radix-ui/themes';
+import { DimensionsIcon, FaceIcon } from '@radix-ui/react-icons';
+import { Button, Flex, Popover, Select, Slider } from '@radix-ui/themes';
 import { Circle } from '@uiw/react-color';
 import { FC } from 'react';
-import './kawaiiList.css';
+import { capitalizeFirstLetter } from '~/utils/capitalizeFirstLetter';
+import './kawaiiListToolbar.css';
 
 type KawaiiListToolbar = {
   color: string;
@@ -27,20 +29,39 @@ export const KawaiiListToolbar: FC<KawaiiListToolbar> = ({
   setSize
 }) => {
   return (
-    <Flex gap="4" position="sticky" top="0">
+    <Flex className="kawaiiListToolbar" gap="4" position="sticky" top="0">
       <Flex direction="column" gap="1">
-        <Circle colors={colors} color={color} onChange={onChangeColor as any} />
+        <Popover.Root>
+          <Popover.Trigger>
+            <Button variant="soft" color="gray" highContrast>
+              <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" width="16px" height="16px">
+                <circle cx="50" cy="50" r="50" fill={color} />
+              </svg>
+              Color
+            </Button>
+          </Popover.Trigger>
+          <Popover.Content className="kawaiiListToolbar__colorPopover">
+            <Flex gap="3">
+              <Circle colors={colors} color={color} onChange={onChangeColor as any} />
+            </Flex>
+          </Popover.Content>
+        </Popover.Root>
       </Flex>
 
       <Flex direction="column" gap="1">
         <Select.Root defaultValue={mood} onValueChange={setMood}>
-          <Select.Trigger />
+          <Select.Trigger variant="soft" color="gray">
+            <Flex as="span" align="center" gap="2">
+              <FaceIcon />
+              {capitalizeFirstLetter(mood)}
+            </Flex>
+          </Select.Trigger>
           <Select.Content>
             <Select.Group>
               <Select.Label>Moods</Select.Label>
               {moods.map((mood, index) => (
                 <Select.Item key={index} value={mood as any}>
-                  {mood}
+                  {capitalizeFirstLetter(mood)}
                 </Select.Item>
               ))}
             </Select.Group>
@@ -49,7 +70,19 @@ export const KawaiiListToolbar: FC<KawaiiListToolbar> = ({
       </Flex>
 
       <Flex direction="column" gap="1">
-        <Slider min={100} max={280} value={size} onValueChange={setSize} />
+        <Popover.Root>
+          <Popover.Trigger>
+            <Button variant="soft" color="gray" highContrast>
+              <DimensionsIcon />
+              Adjust size
+            </Button>
+          </Popover.Trigger>
+          <Popover.Content width="360px">
+            <Flex gap="3">
+              <Slider min={100} max={280} value={size} onValueChange={setSize} />
+            </Flex>
+          </Popover.Content>
+        </Popover.Root>
       </Flex>
     </Flex>
   );
--- apps/website/src/constants/index.ts
+++ apps/website/src/constants/index.ts
@@ -1,14 +1,13 @@
 export const COLORS = [
-  "#FCCB7E",
-
-  "#83D1FB",
-  "#A6E191",
-  "#FDA7DC",
-  "#dac4f7",
-  "#E0E4E8",
+  '#A6E191', // green
+  '#FFB3BA', // pastel red
+  '#FCCB7E', // yellow
+  '#ADD2FF', // blue
+  '#D7BAFF', // pastel purple
+  '#FFBAE1' // pastel magenta,
 ];
 
 export const SPACES = {
   marginY: 40,
-  gutterX: 20,
+  gutterX: 20
 };
