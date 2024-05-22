1171ea5b172f63cb582e9001db18effcdd42c985
Elizabet Oliveira <2750668+miukimiu@users.noreply.github.com>
2024-05-22 16:52:56 +0100

    Favicons and logo link

--- apps/website/src/components/header/Header.tsx
+++ apps/website/src/components/header/Header.tsx
@@ -1,18 +1,22 @@
 import { Button, Container, Flex, Link, Text } from '@radix-ui/themes';
+import NextImage from 'next/image';
+import NextLink from 'next/link';
 import { ButtonGitHubStars } from '~/components/button/ButtonGithubStars';
 import { ButtonThemeToggle } from '~/components/button/ButtonThemeToggle';
+import './header.css';
 
 export const Header = () => {
   return (
-    <Flex width="100%" height="64px" align="center">
+    <Flex width="100%" height="64px" align="center" className="header">
       <Container size="4">
         <Flex justify="between" px="4" gap="4">
           <Flex gap="4" align="center">
-            <Link href="#">
+            <NextLink href="/" className="logo">
+              <NextImage src="/logo.svg" alt="Postgres" width={40} height={40} />
               <Text weight="medium" size="5" color="gray" highContrast style={{ fontFamily: 'var(--font-baloo)' }}>
                 React Kawaii
               </Text>
-            </Link>
+            </NextLink>
 
             <Button color="gray" variant="soft" size="1">
               <Text weight="bold">v.0.18.0</Text>
