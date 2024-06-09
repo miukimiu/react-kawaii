abbe9055d3921eba80fc5c0f598ce72a8a8b32af
Elizabet Oliveira <2750668+miukimiu@users.noreply.github.com>
2024-06-09 22:52:31 +0100

    Enhance `buttonThemeToggle`

--- apps/website/src/components/button/ButtonThemeToggle.tsx
+++ apps/website/src/components/button/ButtonThemeToggle.tsx
@@ -2,14 +2,23 @@
 import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
 import { IconButton } from '@radix-ui/themes';
 import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
+import './buttonThemeToggle.css';
 
 export const ButtonThemeToggle = () => {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const isDarkTheme = theme === 'dark';
 
   return (
     <IconButton
+      className="buttonThemeToggle"
       color="gray"
       variant="soft"
       size="1"
@@ -18,7 +27,13 @@ export const ButtonThemeToggle = () => {
         setTheme(isDarkTheme ? 'light' : 'dark');
       }}
     >
-      {isDarkTheme ? <SunIcon width="18" height="18" /> : <MoonIcon width="18" height="18" />}
+      {!mounted ? (
+        <AiOutlineLoading3Quarters className="buttonThemeToggle__loading" />
+      ) : isDarkTheme ? (
+        <SunIcon width="18" height="18" />
+      ) : (
+        <MoonIcon width="18" height="18" />
+      )}
     </IconButton>
   );
 };
