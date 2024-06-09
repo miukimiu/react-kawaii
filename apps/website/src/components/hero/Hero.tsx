a30a5e859b39172feb2645acbe7aff18cc604846
Elizabet Oliveira <2750668+miukimiu@users.noreply.github.com>
2024-06-09 22:26:53 +0100

    Adding get started button

--- apps/website/src/components/hero/Hero.tsx
+++ apps/website/src/components/hero/Hero.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { Box, Container, Flex, Heading, Text } from '@radix-ui/themes';
+import { Box, Button, Container, Flex, Heading, Text } from '@radix-ui/themes';
+import NextLink from 'next/link';
 import './hero.css';
 
 export const Hero = () => {
   return (
-    <Container size="2" className="hero" pb="4">
+    <Container size="2" className="hero" pb="8">
       <Box px="4">
         <Flex direction="column" align="center" justify="center" gap="4" minHeight="240px">
           <Heading
@@ -22,6 +23,13 @@ export const Hero = () => {
             React Kawaii is an open source library of cute SVG illustrations. Perfect if you want to give some cuteness
             to your React App.
           </Text>
+
+          <NextLink
+            href="https://github.com/miukimiu/react-kawaii/blob/main/packages/react-kawaii/README.md"
+            className="header__link"
+          >
+            <Button size="3">Get started</Button>
+          </NextLink>
         </Flex>
       </Box>
     </Container>
