fd7a7fcfbcf1bdebe28ce52ae58d3e5dac505880
Elizabet Oliveira <2750668+miukimiu@users.noreply.github.com>
2024-08-18 00:23:38 +0100

    Remove inter. Add open graph.

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -9,12 +9,6 @@ import { Footer } from '~/components/footer/Footer'; // Import the Footer compon
 import { Header } from '~/components/header/Header';
 import '../global.css';
 
-const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-  display: 'swap'
-});
-
 const baloo = Baloo_2({
   weight: ['800'],
   subsets: ['latin'],
@@ -23,24 +17,31 @@ const baloo = Baloo_2({
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://design.xata.io'),
   title: 'React Kawaii',
   description:
-    'React Kawaii is a library of cute SVG illustrations (react components). Ideal if you want to give some cuteness and personality to your react application.',
+    'React Kawaii is an open source library of cute SVG illustrations. Perfect if you want to give some cuteness to your React App.',
   twitter: {
-    card: 'summary_large_image'
+    card: 'summary_large_image',
+    creator: '@miuki_miu'
   },
   openGraph: {
     type: 'website',
     title: 'React Kawaii',
     description:
-      'React Kawaii is a library of cute SVG illustrations (react components). Ideal if you want to give some cuteness and personality to your react application.'
+      'React Kawaii is an open source library of cute SVG illustrations. Perfect if you want to give some cuteness to your React App.',
+    images: [
+      {
+        url: '/images/react-kawaii-open-graph.png',
+        width: 1200,
+        height: 630
+      }
+    ]
   }
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${inter.variable} ${baloo.variable}`}>
+    <html lang="en" className={`${baloo.variable}`}>
       <body className="body">
         <NextThemesProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <Theme accentColor="violet" grayColor="gray">
